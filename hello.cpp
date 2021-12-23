68c18e4cbd77f88a2e0bb06e766b486a299bb001
Usama Ahmad <ahmadusama8000@gmail.com>
2021-12-23 21:14:21 +0500

    duniya

--- hello.cpp
+++ hello.cpp
@@ -2,5 +2,6 @@
 using namespace std;
 int main() {
    cout << "Hello, World!" << endl; // This prints Hello, World!
+   cout << "Hello, duniya!" << endl; // This prints Hello, World!
    return 0;
 }
